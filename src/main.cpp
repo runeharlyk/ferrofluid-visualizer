e984a5c6bbab1412a384630ac48fa06861004041
Rune Harlyk <rune@harlyk.dk>
2025-01-04 22:37:37 +0100

    🎵 Adds fft

--- src/main.cpp
+++ src/main.cpp
@@ -18,15 +18,26 @@ TBlendType currentBlending;
 
 static const char *TAG = "main";
 
+#define CEIL(x) ((x) == (int)(x) ? (x) : ((x) > 0 ? (int)(x) + 1 : (int)(x)))
+
 #define I2S_PORT I2S_NUM_0
 
-#define bufferLen 64
-#define SAMPLES 1024
+#define N 64
 #define SAMPLE_RATE 16000 // 44100
+#define RESOLUTION (SAMPLE_RATE / N)
 
-int16_t sBuffer[bufferLen];
+#define LOWPASS 150
+#define BIN_RANGE (CEIL(LOWPASS / RESOLUTION) + 1)
+
+int16_t sBuffer[N];
+
+float fft_table[2 * N];
+float fft_data[2 * N];
+float fft_result[N / 2];
+__attribute__((aligned(16))) float wind[N];
 
 float mean = 0;
+float range_amplitude = 0.0f;
 
 Biquad bq;
 
@@ -49,7 +60,7 @@ void setup() {
       .setCorrection(TypicalLEDStrip);
 
   BiquadInit(&bq);
-  BiquadLowpass(&bq, 0.707, 500, SAMPLE_RATE);
+  BiquadLowpass(&bq, 0.707, LOWPASS, SAMPLE_RATE);
 
   delay(1000);
   pinMode(EN_PIN, OUTPUT);
@@ -57,7 +68,9 @@ void setup() {
 
   esp_err_t ret;
   ESP_LOGI(TAG, "Start Example.");
-  ret = dsps_fft2r_init_fc32(NULL, CONFIG_DSP_MAX_FFT_SIZE);
+  dsps_wind_hann_f32(wind, N);
+  ret = dsps_fft2r_init_fc32(NULL, N);
+
   if (ret != ESP_OK) {
     ESP_LOGE(TAG, "Not possible to initialize FFT. Error = %i", ret);
     return;
@@ -71,7 +84,7 @@ void setup() {
       .communication_format = i2s_comm_format_t(I2S_COMM_FORMAT_STAND_I2S),
       .intr_alloc_flags = 0,
       .dma_buf_count = 8,
-      .dma_buf_len = bufferLen,
+      .dma_buf_len = N,
       .use_apll = false};
 
   i2s_driver_install(I2S_PORT, &i2s_config, 0, NULL);
@@ -90,8 +103,8 @@ void setup() {
 
 void loop() {
   size_t bytesIn = 0;
-  esp_err_t result =
-      i2s_read(I2S_PORT, &sBuffer, bufferLen, &bytesIn, portMAX_DELAY);
+  esp_err_t result = i2s_read(I2S_PORT, &sBuffer, N * sizeof(int16_t), &bytesIn,
+                              portMAX_DELAY);
 
   if (result != ESP_OK)
     return;
@@ -106,10 +119,36 @@ void loop() {
 
   for (int16_t i = 0; i < samples_read; ++i) {
     float s = static_cast<float>(sBuffer[i]);
+    fft_data[2 * i] = s * wind[i]; // Real part
+    fft_data[2 * i + 1] = 0;       // Imaginary part
     float temp = BiquadUpdate(&bq, s);
     sum += temp * temp;
     newMean += temp;
   }
+
+  dsps_fft2r_fc32(fft_data, N);
+  dsps_bit_rev_fc32(fft_data, N);
+
+  for (int i = 0; i < N / 2; i++) {
+    float real = fft_data[2 * i];
+    float imag = fft_data[2 * i + 1];
+    fft_result[i] = sqrt(real * real + imag * imag) / 32768.f * 255.f;
+    // Serial.printf("255, 0, %f, ", fft_result[i] / 32768.f * 255.f);
+  }
+
+  float new_range_amplitude = 0.0f;
+
+  for (int i = 1; i < N / 2; i++) {
+    new_range_amplitude = max(new_range_amplitude, fft_result[i]);
+  }
+
+  new_range_amplitude = min(new_range_amplitude * 3, 255.f);
+
+  range_amplitude = (new_range_amplitude > range_amplitude)
+                        ? new_range_amplitude
+                        : lerp(range_amplitude, new_range_amplitude, 0.1);
+
+  Serial.println(new_range_amplitude);
 
   sum = min(sqrt(sum / samples_read), 255.f);
 
@@ -117,9 +156,9 @@ void loop() {
 
   newMean = min(abs(newMean), 255.f);
 
-  mean = (newMean > mean) ? newMean : lerp(mean, newMean, 0.2);
+  mean = (newMean > mean) ? newMean : lerp(mean, newMean, 0.1);
 
-  Serial.printf("%f, %f, %f\n", sum, mean, newMean);
+  // Serial.printf("%f, %f, %f\n", sum, mean, newMean);
 
-  analogWrite(EN_PIN, static_cast<int>(newMean));
+  analogWrite(EN_PIN, static_cast<int>(range_amplitude));
 }
